b79d7d67d9dbfe28568521eeb0be6f886a417cf8
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2022-09-22 16:04:57 -0400

    Add flip horizontal do player

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,8 +1,9 @@
 class_name Player
 extends KinematicBody2D
 
-var speed := 20
+var speed := 40
 var _velocity := Vector2.ZERO
+var _flip_h:= false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,3 +17,9 @@ func _move(delta: float) -> void:
 		)
 	
 	_velocity = move_and_slide(_velocity * speed * delta * 100)
+	
+	if _velocity.x == 0:
+		$Sprite.flip_h = _flip_h
+	else:
+		_flip_h = _velocity.x < 0
+		$Sprite.flip_h = _flip_h
