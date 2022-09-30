93ca5214342690f8d1b7eeb4a284a7d5f7e946b5
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2022-09-30 10:34:30 -0400

    Alterado animacao da HealthBar
    Add uma animacao mais fluida ao receber dano.

--- src/scripts/interface/health_bar_enemie.gd
+++ src/scripts/interface/health_bar_enemie.gd
@@ -1,10 +1,14 @@
 extends Control
 
+onready var white_bar: TextureProgress = $WhiteBar
+onready var red_bar: TextureProgress = $RedBar
 
 func update_health(health: int) -> void:
-	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT_IN)
-	tween.tween_property($TextureProgress, "value", float(health), 0.3)
+	var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT_IN)
+	red_bar.value = health
+	tween.tween_property(white_bar, "value", float(health), 0.3)
 
 
 func set_max_health(health: int) -> void:
-	$TextureProgress.max_value = health
+	red_bar.max_value = health
+	white_bar.max_value = health
